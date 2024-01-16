074616a833774053602d58793bc09daded7ab77d
minhquang <badboy090799@gmail.com>
2024-01-16 11:02:17 +0700

    add file HTML

--- project.crud.docfile/src/main/java/app/configuration/WebConfig.java
+++ project.crud.docfile/src/main/java/app/configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081") 
+                .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin",
                         "Access-Control-Request-Method", "Access-Control-Request-Headers")
